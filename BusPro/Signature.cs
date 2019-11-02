27ffdaaa95dd355719014ba8cc24499b418d8382
Paweł Jasiński <p.jasinski.2@stud.elka.pw.edu.pl>
2019-11-02 15:52:06 +0100

    poprawilem funkcje checkByte i dodalem funkcje zwracajaca tablice bajtow podpisu

--- BusPro/Signature.cs
+++ BusPro/Signature.cs
@@ -20,10 +20,14 @@ namespace BusPro
     class Signature
     {
         private readonly Byte[] PrivateKey1 = new Byte[8192]; // Also final signature//maybe
-        private readonly Byte[] PrivateKey2 = new Byte[8192];
+        private Byte[] PrivateKey2 = new Byte[8192];
         private readonly Byte[] PublicKey1 = new Byte[8192];
         private readonly Byte[] PublicKey2 = new Byte[8192];
         private Byte[] Hash;//32 bytes
+
+        public byte[] Public1 => PublicKey1;
+
+        public byte[] Public2 => PublicKey2;
 
         public Signature()//generating private keys
         {
@@ -36,7 +40,7 @@ namespace BusPro
             using (SHA256 mySHA = SHA256.Create())
             {
                 Byte[] temp;
-                for(int i=0; i<=255; i++)
+                for(int i=0; i<256; i++)
                 {
                     temp = Support.SubArray<Byte>(PrivateKey1, 32 * i, 32);//substract 256bit number(32*8)
                     temp = mySHA.ComputeHash(temp);//hashing
@@ -45,7 +49,7 @@ namespace BusPro
                         PublicKey1[32 * i + j] = temp[j];
                     }
                 }
-                for (int i = 0; i <= 255; i++)
+                for (int i = 0; i < 256; i++)
                 {
                     temp = Support.SubArray<Byte>(PrivateKey2, 32 * i, 32);
                     temp = mySHA.ComputeHash(temp);
@@ -73,14 +77,33 @@ namespace BusPro
         }
         private void CheckByte(int x) // creating signature based on hash (need to be used for each hash byte)
         {
-            if ((Hash[x] & 0b_1000_0000) == 0) PrivateKey1[x + 0] = PrivateKey2[x + 0];
-            if ((Hash[x] & 0b_0100_0000) == 0) PrivateKey1[x + 1] = PrivateKey2[x + 1];
-            if ((Hash[x] & 0b_0010_0000) == 0) PrivateKey1[x + 2] = PrivateKey2[x + 2];
-            if ((Hash[x] & 0b_0001_0000) == 0) PrivateKey1[x + 3] = PrivateKey2[x + 3];
-            if ((Hash[x] & 0b_0000_1000) == 0) PrivateKey1[x + 4] = PrivateKey2[x + 4];
-            if ((Hash[x] & 0b_0000_0100) == 0) PrivateKey1[x + 5] = PrivateKey2[x + 5];
-            if ((Hash[x] & 0b_0000_0010) == 0) PrivateKey1[x + 6] = PrivateKey2[x + 6];
-            if ((Hash[x] & 0b_0000_0001) == 0) PrivateKey1[x + 7] = PrivateKey2[x + 7];
+            //if ((Hash[x] & 0b_1000_0000) == 0) PrivateKey1[x + 0] = PrivateKey2[x + 0];
+            if ((Hash[x] & 0b_1000_0000) != 0) TransferBytes(x, 0);
+            if ((Hash[x] & 0b_0100_0000) != 0) TransferBytes(x, 1);
+            if ((Hash[x] & 0b_0010_0000) != 0) TransferBytes(x, 2);
+            if ((Hash[x] & 0b_0001_0000) != 0) TransferBytes(x, 3);
+            if ((Hash[x] & 0b_0000_1000) != 0) TransferBytes(x, 4);
+            if ((Hash[x] & 0b_0000_0100) != 0) TransferBytes(x, 5);
+            if ((Hash[x] & 0b_0000_0010) != 0) TransferBytes(x, 6);
+            if ((Hash[x] & 0b_0000_0001) != 0) TransferBytes(x, 7);
+        }
+
+        private void TransferBytes(int x, int y)
+        {
+            Byte[] temp;
+            temp = Support.SubArray<Byte>(PrivateKey2, (32 * x + y) * 32, 32);
+            for (int j = 0; j < 32; j++)
+                PublicKey1[32 * x + j] = temp[j];
+        }
+
+        public Byte[] CreateSignature()
+        {
+            for(int i = 0; i<256; i++)
+            {
+                CheckByte(i);
+            }
+            PrivateKey2 = new Byte[8192];
+            return PrivateKey1;
         }
     }
 }
